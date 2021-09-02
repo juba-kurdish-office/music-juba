3d3b72d3949069bf41f64d78f3b1ddf5e7438ebc
juba kurdish office <87152369+juba-kurdish-office@users.noreply.github.com>
2021-09-02 23:24:52 +0300

    Update volume.js

--- Music/volume.js
+++ Music/volume.js
@@ -28,7 +28,7 @@ execute(message, args) {
     if (!canModifyQueue(message.member)) return;
     //define Info Embed
     const volinfoembed = new MessageEmbed()
-    .setColor("#FF0000")
+    .setColor("#FF55FF")
     .setTitle(`<a:emoji_90:882032776591835208> Volume is: \`${queue.volume}%\``)
     //if no args return info embed
     if (!args[0]) return message.channel.send(volinfoembed).catch(console.error);
@@ -43,7 +43,7 @@ execute(message, args) {
     queue.connection.dispatcher.setVolumeLogarithmic(args[0] / 100);
     //define approve embed
     const volinfosetembed = new MessageEmbed()
-    .setColor("#FF0000")
+    .setColor("#FF55FF")
     .setTitle(`<a:emoji_90:882032776591835208> Volume changed to: \`${args[0]}%\`!`)
     //Send approve message
     return queue.textChannel.send(volinfosetembed).catch(console.error);
