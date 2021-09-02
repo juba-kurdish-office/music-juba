87fdd9fdd075459b4f2b43df7bb5d9800a6d3fc8
juba kurdish office <87152369+juba-kurdish-office@users.noreply.github.com>
2021-09-02 23:00:41 +0300

    Update skip.js

--- Music/skip.js
+++ Music/skip.js
@@ -19,7 +19,7 @@ execute(message) {
     //if not in a guild retunr
     if (!message.guild) return;
     //react with approve emoji
-    message.react("<:emoji_4:815583574983966720>").catch(console.error);
+    message.react("<a:emoji_91:882033133753597992>").catch(console.error);
     //get the queue
     const queue = message.client.queue.get(message.guild.id);
     //if no Queue return error
