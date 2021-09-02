0fb0ef694b0382ef4686573e6c4a45e977361ebd
juba kurdish office <87152369+juba-kurdish-office@users.noreply.github.com>
2021-09-02 23:14:11 +0300

    Update ping.js

--- others/ping.js
+++ others/ping.js
@@ -13,8 +13,8 @@ module.exports = {
   edesc: "Type this command to see how fast the Bot can response to your messages / commands inputs!",
   execute(message, args, client) {
     //react with approve emoji
-    message.react("<a:emoji_18:818844275750993940>");
+    message.react("<:emoji_81:881156629742096394>");
     //send the Ping embed
-    message.reply(new MessageEmbed().setColor("#FF0000").setTitle("<a:emoji_25:819433282356641832> `" + client.ws.ping + "ms`"));
+    message.reply(new MessageEmbed().setColor("#FF55FF").setTitle("<a:emoji_25:819433282356641832> `" + client.ws.ping + "ms`"));
   }
 }
