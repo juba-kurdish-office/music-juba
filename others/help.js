38c82890b42733bd37d1b87c6e085e8a49c44d10
juba kurdish office <87152369+juba-kurdish-office@users.noreply.github.com>
2021-09-02 23:29:13 +0300

    Update help.js

--- others/help.js
+++ others/help.js
@@ -30,7 +30,7 @@ module.exports = {
 > \`lock(l) . unlock(ul) . ban . unban . slowmode . giveaway . cv . mute . unmute . say . bans . clear\`
 
 <a:emoji_5:872571053086503012>┃ **Links**
-[support](https://discord.gg/dC8tuEczZb)    -    [invite](https://discord.com/api/oauth2/authorize?client_id=881148979373555763&permissions=261724045121&scope=bot)
+[support](https://discord.gg/urSTKbdQ7e)    -    [invite](https://discord.com/api/oauth2/authorize?client_id=881148979373555763&permissions=261724045121&scope=bot)
 `)
 
    .setFooter(`${message.author.username}#${message.author.discriminator}`, message.member.user.displayAvatarURL({ dynamic: true }))
