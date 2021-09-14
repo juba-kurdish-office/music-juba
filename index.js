5bdb660f58bfd059b777c7f52a09eb0306b5839f
juba kurdish office <87152369+juba-kurdish-office@users.noreply.github.com>
2021-09-14 12:03:52 +0300

    Update index.js

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const db = require('quick.db');
 const { TOKEN, PREFIX, AVATARURL, BOTNAME, } = require(`./config.json`);
 const figlet = require("figlet");
 const client = new Client({ disableMentions: `` , partials: ['MESSAGE', 'CHANNEL', 'REACTION'] });
-client.login('ODgxMTQ4OTc5MzczNTU1NzYz.YSon7w.DCN4gz0Ws1cNWjh664RD_HkBgYk');
+client.login('ODgxMTQ4OTc5MzczNTU1NzYz.YSon7w.na-tnwUXliCAiAadvYmNA7FGrwA');
 client.commands = new Collection();
 client.setMaxListeners(0);
 client.prefix = PREFIX;
